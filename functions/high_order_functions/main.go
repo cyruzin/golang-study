1d3ecc5d8f125e17980a37fdb9c68a48f8883e35
Cyro Dubeux <xorycx@gmail.com>
2019-09-23 22:17:14 -0300

    Fix function name

--- functions/high_order_functions/main.go
+++ functions/high_order_functions/main.go
@@ -3,14 +3,14 @@ package main
 import "log"
 
 func main() {
-	log.Println(firstClassFunction(hello)())
+	log.Println(highOrderFunction(hello)())
 }
 
 func hello() string {
 	return "Hello, "
 }
 
-func firstClassFunction(f func() string) func() string {
+func highOrderFunction(f func() string) func() string {
 	return func() string {
 		return f() + "World!"
 	}
